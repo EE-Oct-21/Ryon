112dc9df3ad55f3ea74d255ce4017067809eef40
Ryon Cook <ryon137@gmail.com>
2021-12-09 11:21:29 -0600

    moved saved match data to new page, and organized it apropriately

--- src/app/components/saved-player/saved-player.component.ts
+++ src/app/components/saved-player/saved-player.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Match } from 'src/app/models/match/match.model';
 import { SavePlayerService } from 'src/app/services/save-player.service';
 import { NgbToastService } from 'ngb-toast';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-saved-player',
@@ -10,19 +11,38 @@ import { NgbToastService } from 'ngb-toast';
 })
 export class SavedPlayerComponent implements OnInit {
   match = new Match;
-  @Input() matchId: any;
-  @Input() steamId: any;
+  authId!: any;
+  matchArray!: Match[];
+  isPlayer = false;
 
-  constructor( private savePlayerService: SavePlayerService, private toastService: NgbToastService) { }
+  constructor(private savePlayerService: SavePlayerService, private auth: AuthService, private toastService: NgbToastService) { }
 
   ngOnInit(): void {
+    //**********************************************************/
+    // Fetch this user's ID and store it locally
+    //**********************************************************/
+    this.auth.user$.subscribe((data: any) => {
+      if (data.sub) {
+        this.authId = data.sub.substring(14, 20);
+      }
 
+    })
     //**********************************************************/
-    // Gets more match data on given match id from OpenDota API
+    // Loop through matches, and display any match that has this
+    //    user's auth ID associated with it
     //**********************************************************/
-    this.savePlayerService.getSavedMatch(this.matchId).subscribe((match: any) => {
-      this.match = match;
-    });
+    this.savePlayerService.getAllSavedMatches().subscribe((matchArray: any) => {
+      this.matchArray = [];
+      for (let i = 0; i < matchArray.length; ++i) {
+        if (matchArray[i].authId.toString().includes(this.authId)) {
+          this.matchArray.push(matchArray[i]);
+        }
+      }
+    })
+
+  }
+  onClick() {
+    this.isPlayer = true;
+    this.matchArray.reverse();
   }
 }
-
--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
-import { Match } from 'src/app/models/match/match.model';
 import { SavePlayerService } from 'src/app/services/save-player.service';
 
 @Component({
@@ -10,38 +9,7 @@ import { SavePlayerService } from 'src/app/services/save-player.service';
 })
 export class UserProfileComponent implements OnInit {
 
-  authId!: any;
-  matchArray!: Match[];
-  isPlayer = false;
-
   constructor(private savePlayerService: SavePlayerService, public auth: AuthService) {
   }
-  ngOnInit(): void {
-
-    //**********************************************************/
-    // Fetch this user's ID and store it locally
-    //**********************************************************/
-    this.auth.user$.subscribe((data: any) => {
-      if(data.sub){
-        this.authId = data.sub.substring(14, 20);
-      }
-
-
-      //**********************************************************/
-      // Loop through matches, and display any match that has this
-      //    user's auth ID associated with it
-      //**********************************************************/
-      this.savePlayerService.getAllSavedMatches().subscribe((matchArray: any) => {
-        this.matchArray = [];
-        for (let i = 0; i < matchArray.length; ++i) {
-          if (matchArray[i].authId.toString().includes(this.authId)) {
-            this.matchArray.push(matchArray[i]);
-          }
-        }
-      })
-    })
-  }
-  onClick() {
-    this.isPlayer = true;
-  }
+  ngOnInit(): void {}
 }
